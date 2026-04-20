be35fd799fd34d534c9253662bc62260219c738f
julia15marie <jaubry@mcad.edu>
2026-04-19 21:57:20 -0500

    Add files via upload

---
+++ week12/assignment12/src/data/projects.js
@@ -0,0 +1,71 @@
+export const projects=[{
+  id: 1,
+  name: "Portrait",
+  description: "This code creates a portrait of what I consider a celebrity, the Labubu doll. Canvas 400x400, shapes including Circle, Ellipse, Triangle, Rectangle, and Arc, and downloads a png when key 'd' is pressed. p5.js Reference utilized as a resource for the shapes used in code, this is something I will continue to use in future challenges.",
+  url: "https://editor.p5js.org/julia15marie/sketches/CDlAxnler",
+  image: "gifs/Assignment1.png"
+},
+{
+  id: 2,
+  name: "Variables",
+  description: "This code show's my portrait from last week with an added interaction. I chose to add variables to the pupils of the eyes, having them move back and forth horizontally. This code also saves a gif from pressing key 's'. ",
+  url: "https://editor.p5js.org/julia15marie/sketches/WL0NygOVt",
+  image: "gifs/Assignment2.gif"
+},
+{
+  id: 3,
+  name: "Interaction of Abstraction",
+  description: "This week's code assignment uses random to change the color of the cubes on every click. I created a cube function to duplicate the cube in a different location.",
+  url: "https://editor.p5js.org/julia15marie/sketches/4G-BJ07bg",
+  image: "gifs/Assignment3.gif"
+},
+{
+  id: 4,
+  name: "One Player Pong",
+  description: "My code is for the one player pong option. It uses if statements to make sure the ball bounces off of the side and top walls, while making sure it also bounces off and reverses direction after hitting the bar.",
+  url: "https://editor.p5js.org/julia15marie/sketches/r8p_EUhdN",
+  image: "gifs/Assignment4.gif"
+},
+{
+  id: 5,
+  name: "Chestnut Flyaway",
+  description: "On pressing the mouse, the code generates a bunch of ducks to the canvas. A loop runs on each click that generates 5-15 ducks, using random function to decide on the x value, y value, color, size, and rotation. ",
+  url: "https://editor.p5js.org/julia15marie/sketches/8cyuLUSU3",
+  image: "gifs/Assignment5.gif"
+},
+{
+  id: 6,
+  name: "Andy Warho",
+  description: "This code looks to emulate Andy Warhol. Code adjusting for this one was really fun to play around with, although at times I would change things and I wasn't quite sure what they did. ",
+  url: "https://editor.p5js.org/julia15marie/sketches/jll3Sp7hP",
+  image: "gifs/Assignment6.jpg"
+},
+{
+  id: 7,
+  name: "Madlibs",
+  description: "Allows you to partake in a fun lil madlib by inputting the requested information in the input boxes provided. ",
+  url: "https://editor.p5js.org/julia15marie/sketches/jQ9vZYY2n",
+  image: "gifs/Assignment7.png"
+},
+{
+  id: 8,
+  name: "Data Viz",
+  description: "This code displays data (hours of sleep I got last week) using a chart to visualize.",
+  url: "https://editor.p5js.org/julia15marie/sketches/4plV0FeGJ",
+  image: "gifs/Assignment8.png"
+},
+{
+  id: 9,
+  name: "Country and Capital",
+  description: "My code allows users to search for information. I listed countries that I hope to one day visit!",
+  url: "https://editor.p5js.org/julia15marie/sketches/e3T9v7ePt",
+  image: "gifs/Assignment9.png"
+},
+{
+  id: 10,
+  name: "White Christmas",
+  description: "This code creates snowflakes that fall and wiggle at random on the canvas.",
+  url: "https://editor.p5js.org/julia15marie/sketches/1lx9mFGE_",
+  image: "gifs/Assignment10.gif"
+},
+]
---
+++ week12/assignment12/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import Projects from "../components/Projects";
+
+function Home() {
+    <div>
+        <h1>Welcome to the Home Page!</h1>
+        <Projects />
+    </div>
+}
+
+export default Home;
